12c92e6a1fe00ef1ebf2f07cb586e3f4631ae1e4
globalbusiness-Ecosystem <globalbusiness435@gmail.com>
2026-04-08 20:22:16 -0700

    Move Firebase config to env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx_SliAbdg3mou7p08kR0lq_16KNV5XI1",
-  authDomain: "re-global-2d9bc.firebaseapp.com",
-  projectId: "re-global-2d9bc",
-  storageBucket: "re-global-2d9bc.firebasestorage.app",
-  messagingSenderId: "690182944594",
-  appId: "1:690182944594:web:cf6899b34f9a97ad64ec8b",
-  measurementId: "G-J62M98F7Y4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
